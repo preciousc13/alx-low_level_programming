d286b5b1ca7cbe7612d4903644d5b4b442c9ad63
Precious Chauke <preciouschauke911@gmail.com>
2023-04-20 04:41:30 -0700

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!bin/bash
+#include<studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
