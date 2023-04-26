15778df2723a0300aa92cca8a3e0813c70fcb43f
Precious Chauke <preciouschauke911@gmail.com>
2023-04-26 08:29:15 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * main - Entry point
- * Return: Always 0
+ * Return: Alaways 0
  */
 
 int main(void)
@@ -15,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
